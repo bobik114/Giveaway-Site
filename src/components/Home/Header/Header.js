7427ab7f7d64a6eeacc9bc0619546a0db50928f3
Lukasz <bobryk.lukasz@gmail.com>
2020-04-07 23:37:29 +0200

    header section finished

--- src/components/Home/Header/Header.js
+++ src/components/Home/Header/Header.js
@@ -9,15 +9,16 @@ import {
 import styles from './Header.module.scss';
 import UserNav from './UserNav/UserNav';
 import MainNav from './MainNav/MainNav';
+import Section from './Section/Section';
 import homeHeroImage from '../../../assets/Home-Hero-Image.jpg';
 
 const Header = () => {
     return <div className={styles.container}>
-        <img className={styles.homeHeroImage} src={homeHeroImage}/>
+        <div className={styles.homeHeroImage}/>
         <div className={styles.sideContainer}>
             <UserNav />
             <MainNav />
-            
+            <Section />
         </div>
     </div>   
 };

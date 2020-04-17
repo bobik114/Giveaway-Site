53a73fd78b8cb440b27a779c2d28d2578b54356b
Lukasz <bobryk.lukasz@gmail.com>
2020-04-17 13:39:30 +0200

    added components ThreeColumns, SimpleSteps, AboutUs

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -7,10 +7,18 @@ import {
     NavLink,
   } from 'react-router-dom';
 import Header from './Header/Header';
+import ThreeColumns from './ThreeColumns/ThreeColumns';
+import SimpleSteps from './SimpleSteps/SimpleSteps';
+import AboutUs from './AboutUs/AboutUs'
+import WhoWeHelp from './WhoWeHelp/WhoWeHelp';
 
 const Home = () => {
     return <>
         <Header />
+        <ThreeColumns />
+        <SimpleSteps />
+        <AboutUs />
+        <WhoWeHelp />
     </>   
 };
 
